c4d3003727d6865cc797d55e73071086179f1c5d
xiaobai <1041258672@qq.com>
2021-01-08 17:55:43 +0800

    添加antd select校验问题

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,31 +1,76 @@
 <template>
-  <div class="login">
-    <a-button>123</a-button>
-    <button class="pan-btn pink-btn">123123</button>
+  <div class="menu-set-update">
+    <div class="page-box">
+      <div class="form-box search-box">
+        <div class="form-title">菜单信息</div>
+        <a-form
+          class="form-content"
+          ref="ruleForm"
+          :model="form"
+          :label-col="{ span: 4 }"
+          :wrapper-col="{ span: 14 }"
+        >
+          <a-form-item
+            label="适用范围:"
+            name="scope"
+            v-bind="validateInfos.scope"
+          >
+            <a-select
+              v-model:value="form.scope"
+              :defaultValue="form.scope"
+              placeholder="适用范围"
+            >
+              <a-select-option value="1"> 定制 </a-select-option>
+              <a-select-option value="2"> 通用 </a-select-option>
+            </a-select>
+          </a-form-item>
+          <a-form-item :wrapper-col="{ span: 14, offset: 4 }">
+            <a-button type="primary" @click="onSubmit"> 提交 </a-button>
+            <a-button @click="goBack"> 取消 </a-button>
+          </a-form-item>
+        </a-form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { reactive } from 'vue';
-
+import { reactive, toRaw } from 'vue';
+import { useForm } from '@ant-design-vue/use';
 export default {
+  name: 'MenuSetUpdate',
   setup() {
-    //使用类型注解 更加严谨
-    const loginData = reactive({
-      account: '',
-      password: ''
+    const form = reactive({
+      scope: undefined
     });
-    console.log(loginData);
+    const rulesRef = reactive({
+      scope: [
+        {
+          required: true,
+          message: '请选择适用范围'
+        }
+      ]
+    });
+    const { validate, validateInfos } = useForm(form, rulesRef);
+    const onSubmit = function (e: any) {
+      e.preventDefault();
+      validate()
+        .then(() => {
+          console.log(toRaw(form));
+        })
+        .catch((err) => {
+          console.log('error', err);
+        });
+    };
+    const goBack = function () {
+      console.log('goBack');
+    };
     return {
-      loginData
+      form,
+      validateInfos,
+      onSubmit,
+      goBack
     };
   }
 };
 </script>
-<style lang="less" scoped>
-.login {
-  width: 100%;
-  height: 100%;
-  background-color: @primary-color;
-}
-</style>
